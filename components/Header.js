09d11a68955f3c58e8bcabe2d8ee4f7a4977d251
mirzarad <mirza.radoncic@stonybrook.edu>
2021-10-06 22:25:36 -0400

    Finished Product

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import TitleText from './TitleText';
 import Colors from '../constants/colors';
 
 const Header = props => {
   return (
     <View style={styles.header}>
-      <Text style={styles.headerTitle}>{props.title}</Text>
+      <TitleText>{props.title}</TitleText>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   header: {
@@ -18,10 +20,6 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.primary,
     alignItems: 'center',
     justifyContent: 'center'
-  },
-  headerTitle: {
-    color: 'white',
-    fontSize: 18
   }
 });
 
